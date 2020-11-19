cf880aea2468e253b874385ac87fb067af05c341
socratone <gim2origin@gmail.com>
2020-11-19 21:21:03 +0900

    useContext 사용

--- src/Div.js
+++ src/Div.js
@@ -1,16 +1,12 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { AppContext } from './App';
 
 const Div = () => {
+  const text = useContext(AppContext);
+
   return (
-    <AppContext.Consumer>
-      {text => {
-        return (
-          <div>{text}</div>
-        );
-      }}
-    </AppContext.Consumer>
-  );
+    <div>{text}</div>
+  );  
 };
 
 export default Div;
